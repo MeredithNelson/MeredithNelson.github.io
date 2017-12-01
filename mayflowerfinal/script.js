1424ca62998303d1ea3b765820ee8674cd15cf8e
Meredith Nelson <nelsm796@newschool.edu>
2017-12-01 14:06:59 -0500

    version 2 is up

--- mayflowerfinal/script.js
+++ mayflowerfinal/script.js
@@ -1,23 +1,17 @@
-function arrayLoop(){
+function showAbout(){
+	var about = document.getElementById("about"),
+		menu = document.getElementById("menu");
 
-	var aboutMe = ["I've lived in so many states and so many enviorments.", 
-	"Ranging from the hot heat in Florida to the bitter cold winters in Colorado.", 
-	"In memory of all the states I have lived in I wanted to create a bakery.", 
-	"Each cupcake on the menu represents a place I once called home."];
-        
-        for (var i = 0; i < aboutMe.length; i++) {
-            $("body").append("<p>" + aboutMe[i] + "</p>");
+	about.style.display="block";
 
-}
+	menu.style.display="none";
 }
 
-function arrayLoop2(){
+function menu(){
+	var about = document.getElementById("about"),
+		menu = document.getElementById("menu");
 
-	var menu = ["<img src='images/menu.png'>"];
-        
-        for (var i = 0; i < menu.length; i++) {
-            $("body").append("<p>" + menu[i] + "</p>");
+	about.style.display="none";
 
-}
-
+	menu.style.display="block";
 }
