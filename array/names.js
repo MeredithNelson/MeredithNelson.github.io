7d6a1b8c95b2f672a8bd0cddf92a57382e206441
Meredith Nelson <nelsm796@newschool.edu>
2017-12-08 23:18:07 -0500

    refreshing folders

--- array/names.js
+++ array/names.js
@@ -1,47 +1,8 @@
-var colortring = "yellow, blue, red, green, white, black, silver, brown, gold, purple, orange, indigo, magenta"
+function arrayLoop(){
 
-console.log(colorstring)
-
-var colorArray = [1, 2, 3, 5, 5, null, true]
-
-console.log(colorArray)
-console.log(colorArray.toString() )
-
-var nameArray = colorstring.split(",")
-console.log(colorArray)
-
-
-var container = document.querySelector(".output ul")
-
-// for(var i = 0; i < nameArray.length; i++) {
-// 	console.log(nameArray[i])
-// }
-
-colorArray.forEach(function(theElement, itsIndex, theOriginalArray) {
-	console.log(theElement + " is in position " + itsIndex)
-})
-
-
-colorArray.forEach(make_a_list);
-
-
-function make_a_list(el, ix) {
-	var listItem = document.createElement("li")
-
-	listItem.textContent = el + ": index " + ix
-
-	container.appendChild(listItem)
+	var names = ["sarah", "brad", "charlie", "kris", "alexander", "matthew", "lilly", "emily", "leo", "blake", "mary", "kate"];
+        
+        for (var i = 0; i < names.length; i++) {
+            $("body").append("<p>" + names[i] + "</p>");
 }
-
-
-var color = document.querySelectorAll(".output ul li")
-console.log(names)
-
-
-names.forEach(changecolor)
-
-function changeBackground(el) {
-	el.style.changeBackground = "rgba(100,20,200," + Math.random() + ")";
 }
-
-
